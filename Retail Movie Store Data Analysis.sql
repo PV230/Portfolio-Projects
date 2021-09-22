23ff1f9bca27914779dda9233aec552536499f04
PV230 <85664127+PV230@users.noreply.github.com>
2021-09-22 15:27:54 +0530

    Update Retail Movie Store Data Analysis.sql
    Analyzing and extracting movie store inventory data.

--- Retail Movie Store Data Analysis.sql
+++ Retail Movie Store Data Analysis.sql
@@ -1,6 +1,5 @@
 /* 
-1. My partner and I want to come by each of the stores in person and meet the managers. 
-Please send over the managers’ names at each store, with the full address 
+Showing the managers’ names at each store, with the full address 
 of each property (street address, district, city, and country please).  
 */ 
 
@@ -20,9 +19,8 @@ FROM store
 ;
 	
 /*
-2.	I would like to get a better understanding of all of the inventory that would come along with the business. 
-Please pull together a list of each inventory item you have stocked, including the store_id number, 
-the inventory_id, the name of the film, the film’s rating, its rental rate and replacement cost. 
+Showing a list of stocked inventory items, store_id number, 
+inventory_id, film name, film’s rating, its rental rate and replacement cost. 
 */
 
 SELECT 
@@ -38,8 +36,7 @@ FROM inventory
 ;
 
 /* 
-3.	From the same list of films you just pulled, please roll that data up and provide a summary level overview 
-of your inventory. We would like to know how many inventory items you have with each rating at each store. 
+Showing rating-wise inventory items in each store.
 */
 
 SELECT 
@@ -55,9 +52,7 @@ GROUP BY
 ;
 
 /* 
-4. Similarly, we want to understand how diversified the inventory is in terms of replacement cost. We want to 
-see how big of a hit it would be if a certain category of film became unpopular at a certain store.
-We would like to see the number of films, as well as the average replacement cost, and total replacement cost, 
+Showing number of films, average replacement cost, total replacement cost, 
 sliced by store and film category. 
 */ 
 
@@ -85,8 +80,7 @@ ORDER BY
 ;
 
 /*
-5.	We want to make sure you folks have a good handle on who your customers are. Please provide a list 
-of all customer names, which store they go to, whether or not they are currently active, 
+Showing a list of all customer names, which store they go to, whether or not they are currently active, 
 and their full addresses – street address, city, and country. 
 */
 
@@ -106,10 +100,9 @@ FROM customer
 ;
 
 /*
-6.	We would like to understand how much your customers are spending with you, and also to know 
-who your most valuable customers are. Please pull together a list of customer names, their total 
-lifetime rentals, and the sum of all payments you have collected from them. It would be great to 
-see this ordered on total lifetime value, with the most valuable customers at the top of the list. 
+Showing a list of customer names, their total lifetime rentals,
+and the sum of all payments ordered by total lifetime value, 
+with the most valuable customers at the top of the list. 
 */
 
 SELECT 
@@ -131,10 +124,9 @@ ORDER BY
     ;
     
 /*
-7. My partner and I would like to get to know your board of advisors and any current investors.
-Could you please provide a list of advisor and investor names in one table? 
-Could you please note whether they are an investor or an advisor, and for the investors, 
-it would be good to include which company they work with. 
+Showing a list of advisor and investor names and identifying if 
+they are an investor or an advisor. For the investors, 
+displaying which company they work with. 
 */
 
 SELECT
@@ -154,10 +146,9 @@ SELECT
 FROM advisor;
 
 /*
-8. We're interested in how well you have covered the most-awarded actors. 
-Of all the actors with three types of awards, for what % of them do we carry a film?
-And how about for actors with two types of awards? Same questions. 
-Finally, how about actors with just one award? 
+Showing actors who have won awards, the % of films in the inventory featuring award-winning actors, 
+and actors who have won 1 and 2 awards,
+along their respective % of films in the inventory
 */
 
 SELECT
